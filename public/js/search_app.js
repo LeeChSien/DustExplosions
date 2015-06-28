9fd3d19fed1a3896b12c98d237647e3345f685fa
Jason Lee <chsienlee@gmail.com>
2015-06-28 13:25:04 +0800

    remove requirejs

--- public/js/search_app.js
+++ public/js/search_app.js
@@ -3,8 +3,6 @@
 
   .controller('mainCtrl', ['$scope', '$http',
     function($scope, $http) {
-      var json = require('./data.json');
-
       $scope.model = {foo: 'bar'};
 
       $http.get('./data.json').success(function(result) {
