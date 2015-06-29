8157443928bc4d96f888c5e9b053276b3490dace
Jason Lee <chsienlee@gmail.com>
2015-06-29 09:20:17 +0800

    add hospital

--- public/js/search_app.js
+++ public/js/search_app.js
@@ -1,23 +1,52 @@
 (function() {
-  angular.module('searchApp',[])
+  angular.module('searchApp',['ui.bootstrap'])
 
-  .controller('mainCtrl', ['$scope', '$http',
-    function($scope, $http) {
+  .controller('mainCtrl', ['$scope', '$http', '$modal',
+    function($scope, $http, $modal) {
       $scope.keywords = ""
       $scope.members = [];
+      $scope.hospitals = {};
+
       $scope.lastmodify = '';
 
-      $http.get('http://tonyq.org/kptaipei/api-20150628.php').success(function(result) {
+      $http.get('http://tonyq.org/kptaipei/api-20150628.php')
+      .success(function(result) {
         $scope.members = result.data;
         $scope.lastmodify = result.lastmodify;
       });
 
-      $scope.search = function() {
-        //
-      };
+      $http.get('https://raw.githubusercontent.com/harry2690/psof/gh-pages/hospitals.json')
+      .success(function(result) {
+        for (var i = 0; i < result.data.length; i++) {
+          $scope.hospitals[result.data['醫院名稱']] = result.data[i];
+        }
+      });
 
       $scope.namePipe = function() {
         return ($scope.keywords.length > 1) ? $scope.keywords[0] + '○' + $scope.keywords.substr(2, $scope.keywords.length-2) : $scope.keywords;
+      };
+
+      $scope.openHospitalModal = function (hospital) {
+        var modalInstance = $modal.open({
+          animation: $scope.animationsEnabled,
+          templateUrl: 'hospitalModalContent.html',
+          controller: 'hospitalModalInstanceCtrl',
+          resolve: {
+            hospital: function () {
+              return hospital;
+            }
+          }
+        });
+      };
+    }
+  ])
+
+  .controller('hospitalModalInstanceCtrl', ['$scope', '$modalInstance', 'hospital',
+    function ($scope, $modalInstance, hospital) {
+      $scope.hospital = hospital;
+
+      $scope.close = function () {
+        $modalInstance.dismiss();
       };
     }
   ]);
